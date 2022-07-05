9d6b59831a73477ccfec9aed066a1a6150471e1c
Tuan Ain <2020865508@student.uitm.edu.my>
2022-07-06 05:50:19 +0800

    add comment

--- finalServer.py
+++ finalServer.py
@@ -1,23 +1,23 @@
-import socket
+import socket #import all socket
 
-IP = "192.168.56.105"
-PORT = 8888
+IP = "192.168.56.105" #ip address of server
+PORT = 8888 #port that used
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-socket.bind((IP, PORT))
+socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #socket for ip address and udp
+socket.bind((IP, PORT)) #bind with ip and port client
 
-print('Server Listening At {}'.format(socket.getsockname()))
+print('Server Listening At {}'.format(socket.getsockname())) #get client ip and port
 
-while True:
-    messageBytes, address = socket.recvfrom(2048)
-    messageString = messageBytes.decode('utf-8')
-    print('Received from client {} : {}'.format(address, messageString))
+while True: #while loop
+    messageBytes, address = socket.recvfrom(2048) #receive from client
+    messageString = messageBytes.decode('utf-8') #decode bytes to string
+    print('Received from client {} : {}'.format(address, messageString)) #print message in string from client
     print('\n')
     print(messageString)
-    
-    if messageString == 'E' or messageString == 'e':
+
+    if messageString == 'E' or messageString == 'e': #when client choose to exit
         break
 
 print('Connection Closed')
-socket.close()
+socket.close() #close all the socket
 
