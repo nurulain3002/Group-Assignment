59713c0b1bc303cc5bcbbaeacc5a820ee1513d3d
nurulain3002 <2020483002@student.uitn.edu.my>
2022-07-17 10:14:12 +0000

    update client

--- ClientCalc.py
+++ ClientCalc.py
@@ -1,61 +1,63 @@
 #client
 import socket
 import math
+import sys
+
 ClientSocket = socket.socket()
 host = '192.168.56.109'
 port = 8888
 
-print('Waiting for connection')
+print('\nWaiting for connection')
 try:
     ClientSocket.connect((host, port))
 except socket.error as e:
     print(str(e))
 
 Response = ClientSocket.recv(2048)
-print(Response)
+print(Response.decode("utf-8"))
+
+
+print(		"\t\t\t* *** *** *** OPERATION LIST *** **** *** *\t\t\t\n")
+
+print("   ------------------------------------------------------------------------------")
+print("   | [A]  cm to m Conversion")
+print("   | [B]  m to cm Conversio")
+print("   | [C]  kg to pounds Conversion")
+print("   | [D]  pounds to kg Conversion")
+print("   | [E]  cm to feet Conversion")
+print("   | [F]  feet to cm Conversion")
+print("   | [G]  km to m Conversion ")
+print("   | [H]  m to km Conversion ")
+print("   | [I]  celcius to f Conversion")
+print("   | [J]  f to celcius Conversion")
+print("   | [K]  Addition Operation")
+print("   | [L]  Subtraction Operation")
+print("   | [M]  Subtraction Operation")
+print("   | [N]  Subtraction Operation")
+print("   | [O]  Subtraction Operation")
+print("   | [P]  Subtraction Operation")
+print("   -------------------------------------------------------------------------------")
+
 while True:
-    print("Option:   \n1-cm to m Conversion     \n2-m to cm Conversion   \n3-kg to pounds Conversion    \n4-pounds to kg Conversion   \n5-cm to feet Conversion    \n6-feet to cm Conversion   \n7-km to m Conversion   \n8-m to km Conversion  \n9-celcius to f Conversion \n10-f to celcius Conversion \n11-Addition Operation \n12-Subtraction Operation \n13-Multiplication Operation \n14-Division Operation  \n15-Modulus Operation  \n16-Power Operation  \n17-exit\n")
-    option = input('Enter Your Option: ')
-    ClientSocket.send(str.encode(option))
-    number = input('Enter number: ')
-    ClientSocket.send(str.encode(number))
-    if (option == '1'):
-      print("cm to m Conversion")
-    elif (option == '2'):
-      print("m to cm Conversion")
-    elif (option == '3'):
-      print("kg to pounds Conversion")
-    elif (option == '4'):
-      print("pounds to kg Conversion")
-    elif (option == '5'):
-      print("cm to feet Conversion")
-    elif (option == '6'):
-      print("feet to cm Conversion")
-    elif (option == '7'):
-      print("km to m Conversion")
-    elif (option == '8'):
-      print("m to km Conversion")
-    elif (option == '9'):
-      print("celcius to f Conversion")
-    elif (option == '10'):
-      print("f to celcius Conversion")
-    elif (option == '11'):
-      print("Addition Operation")
-    elif (option == '12'):
-      print("Subtraction Operation")
-    elif (option == '13'):
-      print("Multiplication Operation")
-    elif (option == '14'):
-      print("Division Operation")
-    elif (option == '15'):
-      print("Modulus Operation")
-    elif (option == '16'):
-      print("Power Operation")
-    elif (option == '17'):
-      print("Bye!")
-      ClientSocket.close()
+    
+    option = input('Enter Your Option or Type "EXIT" if you are done :\n ')
+    
+
+    if option == "A" or option == "B" or option == "C" or option == "D" or option == "E" or option == "F" or option == "G" or option == "H" or option == "I" or option == "J" or option == "K" or option == "L" or option == "M" or option == "N" or option == "O" or option == "P" :
+        number = '0'
+        input = option + ":" + number
+        ClientSocket.send(str.encode(input))
+        Response = ClientSocket.recv(2048)
+        print(Response.decode("utf-8"))
+
+    elif option == "EXIT":
+      print ("THANK YOU FOR USING THIS CONVERSION CALCULATOR\n")
+      break
+
     else:
-      print("Invalid option. Try again!")
-    result = ClientSocket.recv(2048)
-    print("Result:",result.decode('utf-8'))
+      print("WRONG INPUT, TRY AGAIN!!")
+      ClientSocket.send(str.encode(input))
+      Response = ClientSocket.recv(2048)
+      print(Response.decode("utf-8"))
+    
 ClientSocket.close()
